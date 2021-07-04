49bbc7436fccb4624ef06b085ce0b90ed454a8b6
Marian Hello <marian.hello@gmail.com>
2021-07-04 21:14:24 +0200

    tax summaries

---
+++ public/precache-manifest.ec593c5eb175dd0e7dcb4d304cf3feea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72c719b16a37c4dda7e9ad1efcd8dfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5abf020f82cf94a3ff7e",
+    "url": "/static/css/2.89e3458b.chunk.css"
+  },
+  {
+    "revision": "050350bbc502f77ff1cc",
+    "url": "/static/css/main.7951f014.chunk.css"
+  },
+  {
+    "revision": "5abf020f82cf94a3ff7e",
+    "url": "/static/js/2.289dbf39.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/static/js/2.289dbf39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810fdb50f6d4308714e3",
+    "url": "/static/js/3.6125f94c.chunk.js"
+  },
+  {
+    "revision": "008e60abd128520991a648a108c4df35",
+    "url": "/static/js/3.6125f94c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b186b7dfbd3b1152011b",
+    "url": "/static/js/4.2b677150.chunk.js"
+  },
+  {
+    "revision": "f475ec6a22369759e200929069e6ddfa",
+    "url": "/static/js/4.2b677150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f7cc70c4a4eceb8a09",
+    "url": "/static/js/5.077ae580.chunk.js"
+  },
+  {
+    "revision": "659fdc4b947209b48bb8",
+    "url": "/static/js/6.07721a0e.chunk.js"
+  },
+  {
+    "revision": "050350bbc502f77ff1cc",
+    "url": "/static/js/main.ed91f1d4.chunk.js"
+  },
+  {
+    "revision": "560f10488ac606a42a20",
+    "url": "/static/js/runtime-main.ffed6766.js"
+  }
+]);
